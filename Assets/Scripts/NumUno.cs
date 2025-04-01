8e433abc5df0cd70870db9c105cf42efb1ba54b6
maxi0302 <111788863+maxi0302@users.noreply.github.com>
2025-04-01 12:10:56 -0300

    Ejercicio 1 guia 2

---
+++ Assets/Scripts/NumUno.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumUno : MonoBehaviour
+{
+    int Num1 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Num1 += 2;
+        Debug.Log(Num1);
+        Num1 *= Num1;
+        Debug.Log(Num1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
