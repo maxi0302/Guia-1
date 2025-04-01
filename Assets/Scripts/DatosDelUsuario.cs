f2434054568a3a245c541237cc284cb149127789
maxi0302 <111788863+maxi0302@users.noreply.github.com>
2025-04-01 12:01:35 -0300

    Ejercicios 2, 3 y 4

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombre;
+    public int edadUsuario;
+    public float alturaUsuario;
+    public bool esDonante;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es "+ nombre + ", tengo " + edadUsuario + ", mido " + alturaUsuario + " metros y es " + esDonante + " que soy donante de órganos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
