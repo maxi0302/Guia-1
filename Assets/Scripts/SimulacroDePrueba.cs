ca5916750936fdcbf58157d19d8d3ccaf710ba2c
maxi0302 <111788863+maxi0302@users.noreply.github.com>
2025-04-04 09:05:41 -0300

    Simulacro de examen

---
+++ Assets/Scripts/SimulacroDePrueba.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulacroDePrueba : MonoBehaviour
+{
+    public string codAnimal;
+    public int dias;
+    int totalG;
+    int totalM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dias >= 3)
+        {
+            if (codAnimal == "G")
+            {
+                totalG = 300 * dias;
+                Debug.Log("Para ese período se necesitan  " + totalG + " gramos de alimento");
+                totalM = totalG / 100 * 80;
+                Debug.Log("$" + totalM);
+
+            }
+            else if (codAnimal == "PP")
+            {
+                totalG = 400 * dias;
+                Debug.Log("Para ese período se necesitan  " + totalG + " gramos de alimento");
+                totalM = totalG / 100 * 80;
+                Debug.Log("$" + totalM);
+
+            }
+            else if (codAnimal == "PG")
+            {
+                totalG = 700 * dias;
+                Debug.Log("Para ese período se necesitan  " + totalG + " gramos de alimento");
+                totalM = totalG / 100 * 80;
+                Debug.Log("$" + totalM);
+
+            }
+            else
+            {
+                Debug.Log("ERROR: inserte un codigo valido");
+            }
+        } else 
+        {
+            Debug.Log("ERROR: la estadia minima es de 3 dias");
+        }
+
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SumaYProducto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumaYProducto : MonoBehaviour
+{
+    int num1;
+    int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        num1 = 4;
+        num2 = 5;
+
+        Debug.Log(num1 + num2);
+        Debug.Log(num1 * num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
