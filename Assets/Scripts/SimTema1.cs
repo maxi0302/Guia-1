9a9ecf1364ffb848a636583065c06eb6f840cb60
maxi0302 <111788863+maxi0302@users.noreply.github.com>
2025-04-08 11:50:51 -0300

    SimTema 1 y 2 completos

---
+++ Assets/Scripts/SimTema1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimTema1 : MonoBehaviour
+{
+    public int dineroInicial;
+    public string divisa;
+    int dineroFinal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dineroInicial >= 1000)
+        {
+            if (divisa == "D")
+            {
+                dineroFinal = dineroInicial / 1000;
+                Debug.Log("El valor de " + dineroInicial + " pesos en dolares es " + dineroFinal);
+            }
+            else if (divisa == "R")
+            {
+                dineroFinal = dineroInicial / 500;
+                Debug.Log("El valor de " + dineroInicial + " pesos en reales es " + dineroFinal);
+            }
+            else if (divisa == "E")
+            {
+                dineroFinal = dineroInicial / 1174;
+                Debug.Log("El valor de " + dineroInicial + " pesos en euro es " + dineroFinal);
+            }
+            else
+            {
+                Debug.Log("Divisa seleccionada no valida");
+            }
+        }
+        else
+        {
+            Debug.Log("El monto minimo es de 1000");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SimTema2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimTema2 : MonoBehaviour
+{
+    public string nombreAlumno;
+    public int año;
+    public string especialidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nombreAlumno == "") {
+            Debug.Log("Error: Nombre no valido");
+
+        } else {
+            if (año <= 0 || año > 5) {
+                Debug.Log("Error: Año no valido");
+
+            } else {
+                if (año < 3) {
+                    Debug.Log("Error: Aun sigues en ciclo basico");
+
+                } else {
+                    if (especialidad != "D" && especialidad != "T" && especialidad != "H" && especialidad != "M" && especialidad != "G") {
+                        Debug.Log("Error: Solo se puede ingresar D, T, H, M, G");
+
+                    } else {
+                        Debug.Log("Muchas gracias " + nombreAlumno + "!");
+
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
